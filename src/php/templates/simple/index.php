764b9302ca1932c0987c1030e475011264aa6c44
Aaron Collegeman <aaron@withfatpanda.com>
2017-12-26 00:42:58 -0500

    Simple templates

--- src/php/templates/simple/index.php
+++ src/php/templates/simple/index.php
@@ -6,11 +6,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-title"
-    content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
+  <meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <?php wp_head() ?>
 </head>
-<body <?php body_class() ?>></body>
+<body <?php body_class() ?>>
+  <?php while (have_posts()): the_post(); ?>
+    <?php the_content() ?>
+  <?php endwhile; ?>
+</body>
 </html>
